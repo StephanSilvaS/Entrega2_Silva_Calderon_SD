22218f5db41dd85d1f3dd9beae278f2ae9485609
DanielCalderonUsach <daniel.calderon.r@usach.cl>
2023-07-09 19:37:14 -0400

    Producer with Kafka

--- producer/src/main/java/com/crypto/producer/ProducerService.java
+++ producer/src/main/java/com/crypto/producer/ProducerService.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -16,11 +20,20 @@ public class ProducerService {
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
-    @Value("${kafka.topic}")
-    private String topic;
-
+    private static final String TOPIC_NAME = "btc";
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
 
     public void ablyAPI() throws AblyException{
+
+        // Configuración del productor
+        Properties props = new Properties();
+        props.put("bootstrap.servers", BOOTSTRAP_SERVERS);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    
+        // Creación del productor
+        Producer<String, String> producer = new KafkaProducer<>(props);
+        
         // Configurar la conexión
         String apiKey = "KMNnHQ.DbzYqQ:Ur6_rOy3cYjKvlazKcIiHNCSW_QkhjtmtCm4sFZ-J98";
         AblyRealtime ably = new AblyRealtime(apiKey);
@@ -29,19 +42,29 @@ public class ProducerService {
         String channelName = "[product:ably-coindesk/crypto-pricing]btc:usd";
         io.ably.lib.realtime.Channel channel = ably.channels.get(channelName);
 
-        // Suscribirse a los mensajes
-        channel.subscribe(message -> {
-            String m = message.data.toString();
+
+        List<String> listaValores = new ArrayList<String>();
+
+        try{
+            channel.subscribe(message -> {
+                String mensaje = message.data.toString();
+                ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, mensaje);
+                producer.send(record);
+                System.out.println("Mensaje enviado con éxito a Kafka.");
+                listaValores.add(mensaje);
+                
+                if(listaValores.size() == 10){
+                    ably.close();
+                    producer.close();
+                }
+                
+                System.out.println(mensaje);
+                
+            });
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
             
-            List<String> listaValores = new ArrayList<String>();
-            listaValores.add(m);
-            
-            if(listaValores.size() == 10){
-                ably.close();
-            }
-            
-            System.out.println(m);
-            kafkaTemplate.send(topic, m);
-        });
+        }
     }
 }
